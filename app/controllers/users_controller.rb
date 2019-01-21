bc9f787f51733e71e5eba40623e3b7d4e5181e1e
dowpm <mcdowpm@gmail.com>
2019-01-21 15:33:54 -0500

    add logic for create the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,20 @@
 class UsersController < ApplicationController
-    # use Rack::Flash
+    use Rack::Flash
 
     get '/signup' do
+        @user = User.new
         erb :'users/signup'
     end
 
     post '/signup' do
+        # raise params.inspect
+        @user = User.new params[:user]
 
+        if @user.save
+            flash[:msg] = "You've been successfully registered"
+            redirect '/'
+        else            
+            erb :'users/signup'
+        end
     end
 end
