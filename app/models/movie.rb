e611aa5dfe5954f6ddbce95ee051758690b9ded6
dowpm <mcdowpm@gmail.com>
2019-01-22 11:51:31 -0500

    add validation for genre_id

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,7 @@ class Movie < ActiveRecord::Base
     belongs_to :user
     belongs_to :genre
     validates :release_date, presence: true
+    validates :genre_id, presence: true
 
     def genre_name
         genre.name 
