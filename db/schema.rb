2bf57e7278eede93d282dcbc8ea151efcb956b43
dowpm <mcdowpm@gmail.com>
2019-01-22 10:28:09 -0500

    create genres table with a coulumn name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_21_222102) do
+ActiveRecord::Schema.define(version: 2019_01_22_152639) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.integer "user_id"
