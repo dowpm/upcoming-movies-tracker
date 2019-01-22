0b04622854d0d87ca74a3e0e84074d7a3e995560
dowpm <mcdowpm@gmail.com>
2019-01-21 22:37:34 -0500

    add route /movies and movies/new

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,15 @@
 class MoviesController < ApplicationController
+    get '/movies' do
+        @movies = Movie.all
+        erb :'movies/index'
+    end
 
+    get '/movies/new' do
+        @movie = Movie.new(user_id: session[:user_id])
+        if logged_in?
+            erb :'movies/new'
+        else
+            redirect to '/login'
+        end
+    end
 end
